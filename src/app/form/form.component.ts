c4b3d115466647610a734f4a4a475e34775a047d
anacorreaa <anacarolcorr07@gmail.com>
2025-03-04 16:04:35 -0300

    Primeiro commit

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-form',
   standalone: false,
@@ -7,5 +8,29 @@ import { Component } from '@angular/core';
   styleUrl: './form.component.css'
 })
 export class FormComponent {
+
+  num1: number = 0;
+  num2: number = 0;
+  resultado: number = 0;
+
+  //somar
+  onClickSoma(){
+    this.resultado = this.num1 + this.num2;
+  }
+  onClickSubtracao(){
+    this.resultado = this.num1 - this.num2;
+  }
+  onClickDivisao(){
+    this.resultado = this.num1 / this.num2;
+  }
+  onClickMultiplicacao(){
+    this.resultado = this.num1 * this.num2;
+  }
+  onClickPotencia(){
+    this.resultado = Math.pow(this.num1, this.num2);
+  }
+  onClickMedia(){
+    this.resultado = (this.num1 + this.num2) / 2;
+  }
 
 }
